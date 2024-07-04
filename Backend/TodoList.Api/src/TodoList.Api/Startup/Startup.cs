185c636d6e59fd8bc8c65806e265aeef66a45e11
dieselkriek <springkaan@gmail.vom>
2024-07-04 17:07:24 +1200

    REfactor folders

--- Backend/TodoList.Api/src/TodoList.Api/Startup/Startup.cs
+++ Backend/TodoList.Api/src/TodoList.Api/Startup/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-namespace TodoList.Api
+namespace TodoList.Api.Startup
 {
     public class Startup
     {
